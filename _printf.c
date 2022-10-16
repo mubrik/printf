bac74d4ed492805bcc3b2f85b590b411ed45bf55
Mubarak Yahaya <mubarakg4u@gmail.com>
2022-10-16 16:41:02 +0100

    blank

--- _printf.c
+++ _printf.c
@@ -43,8 +43,7 @@ int _printf(const char *format, ...)
 		}
 		else
 			handle_buf(buffer, format[i], ibuf), len++;
-		for (ibuf = len; ibuf > 1024; ibuf -= 1024)
-			;
+		for (ibuf = len; ibuf > 1024; ibuf -= 1024);
 	}
 	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
 	return (len);

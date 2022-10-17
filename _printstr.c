f8b182140885f82244ab84fe29d71854134c9969
Mubarak Yahaya <mubarakg4u@gmail.com>
2022-10-17 10:31:17 +0100

    added better null checking to handle_str_format updated readme

--- _printstr.c
+++ _printstr.c
@@ -12,18 +12,21 @@ int handle_str_format(va_list arg_list, char *buffer,
 	char buffer_i, __attribute__((unused)) char *flags)
 {
 	char *string_ptr;
-	char *null_str = "(nil)";
+	char *null_str = "(null)"; /* print this if NULL */
 	int count = 0;
 
 	/* get argument from list */
 	string_ptr = va_arg(arg_list, char *);
+	/* need to check if arg doesnt exist, using this for now */
+	if (null_str[0] == '\0')
+		return (count);
 	/* null check */
-	if (string_ptr == NULL)
+	if (!string_ptr)
 	{
-		/* print nil */
-		while (*null_str != '\0')
+		/* print null */
+		while (null_str[count] != '\0')
 		{
-			add_to_buffer(*null_str, buffer, (buffer_i + count));
+			add_to_buffer(null_str[count], buffer, (buffer_i + count)), count++;
 		}
 		return (count);
 	}
@@ -31,9 +34,8 @@ int handle_str_format(va_list arg_list, char *buffer,
 	while (*string_ptr != '\0')
 	{
 		/* add to buffer */
-		add_to_buffer(*string_ptr, buffer, (buffer_i + count));
+		add_to_buffer(*string_ptr, buffer, (buffer_i + count)), count++;
 		/* incr count and src ptr*/
-		count++;
 		string_ptr++;
 	}
 
--- test/test.c
+++ test/test.c
@@ -1,6 +1,6 @@
 #include <limits.h>
 #include <stdio.h>
-#include "main.h"
+#include "../main.h"
 
 /**
  * main - Entry point
@@ -9,11 +9,16 @@
  */
 int main(void)
 {
-	int len;
-	int len2;
+	int len, len2, nil, nil2;
 
+	nil = _printf("String:[%s]\n");
+	nil2 = printf("String:[%s]\n");
 	_printf("Let's try to printf a simple sentence.\n");
 	printf("Let's try to printf a simple sentence.\n");
+	_printf("Character:[%c]\n", 'H');
+	printf("Character:[%c]\n", 'H');
+	_printf("String:[%s]\n", "I am a string !");
+	printf("String:[%s]\n", "I am a string !");
 	_printf("Sub Characters %c %c %c\n", 'a', 'b', 'c');
 	printf("Sub Characters %c %c %c\n", 'a', 'b', 'c');
 	_printf("Sub String %s %s %s\n", "stringa", "stringb", "stringc");
@@ -24,5 +29,6 @@ int main(void)
 	printf("Numbers Floatt and decimal %f %f %f %d %d %i %i\n", 1255.66, -12.447, 1.6, 2.2, -0.5, '7', -777777); */
 
 	printf("Length c printf: %d, length myprintf: %d\n", len2, len);
+	printf("NULl c printf: %d, NULl myprintf: %d\n", nil2, nil);
 	return (0);
 }
